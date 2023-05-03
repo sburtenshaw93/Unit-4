fb98dbdd75708e1191fcb8c0d6a9a42a4dc6896b
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-05-02 20:58:04 -0600

    finished part 4

--- server.py
+++ server.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from forms import TeamForm, ProjectForm
+from model import User, Team, Project, connect_to_db
 
 app = Flask(__name__)
 
-@app.oute("/")
+app.secret_key = "keep this secret"
+
+user_id = 1
+
+@app.route("/")
 def home():
-    return render_template("home.html")
+    team_form = TeamForm()
+    project_form = ProjectForm()
+    project_form .update_teams(User.query.get(user_id).teams)
+    return render_template("home.html", team_form = team_form, project_form = project_form)
+
+@app.route("/add-team", methods=["POST"])
+def add_team():
+    team_form = TeamForm()
+    
+    if team_form.validate_on_submit():
+        print(team_form.team_name.data)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+@app.route("/add-project", methods=["POST"])
+def add_project():
+    project_form = ProjectForm()
+    project_form.update_teams(User.query.get(user_id).teams)
+    
+    if project_form.validate_on_submit():
+        print(project_form.project_name.data)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug = True)
